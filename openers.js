3ed567cfbd07c5f3e4b051a91a524cfffe6e088c
sebthiertant <sebastien.thiertant@gmail.com>
2025-08-22 22:01:44 +0200

    Improve openers

--- openers.js
+++ openers.js
@@ -5,17 +5,24 @@ const OPENERS = [
   { text: "Oh oui ! C'est le but !", weight: 5 },
   { text: "Incroyable !", weight: 5 },
   { text: "Quel but !", weight: 5 },
-  
+  { text: "Magnifique !", weight: 5 },
+  { text: "Superbe !", weight: 5 },
+  { text: "Hé ! C'est au fond !", weight: 5 },
+
   // Communes (poids 3)
   { text: "Quel bombasso !", weight: 1.5 },
+  { text: "La frappe !", weight: 1.5 },
   { text: "Quel tir !", weight: 1.5 },
   { text: "C'est pas possible ! Quel but !", weight: 1.5 },
+  { text: "Quelle mine !", weight: 1.5 },
+  { text: "C'est dans la lucarne !", weight: 1.5 },
   
   // Moins communes (poids 2)
-  { text: "La minasse !", weight: 0.8 },
-  { text: "La surprise du chef !", weight: 0.8 },
+  { text: "Oh la minasse !", weight: 0.8 },
+  { text: "C'est la surprise du chef !", weight: 0.8 },
   { text: "BADABOUM !", weight: 0.8 },
-  
+  { text: "Aïe Aïe Aïe !", weight: 0.8 },
+
   // Rares (poids 0.1)
   { text: "Po po po !", weight: 0.1 },
   { text: "Quelle merveille !", weight: 0.1 },
@@ -26,19 +33,22 @@ const OPENERS = [
 // Phrases pour parler de l'équipe qui encaisse
 const CONCEDING_OPENERS = [
   // Communes (poids 3) - SANS "défense" pour éviter les doublons
-  { text: "Ça s'écroule derrière !", weight: 3 },
-  { text: "L'arrière-garde qui s'effondre !", weight: 3 },
-  { text: "La charnière qui lâche !", weight: 3 },
-  
+  { text: "Ça s'écroule complètement derrière !", weight: 3 },
+  { text: "La défense qui s'effondre !", weight: 3 },
+  { text: "La charnière qui se troue !", weight: 3 },
+  { text: "Ça va trop vite pour eux derrière !", weight: 3 },
+
   // Moins communes (poids 2)
   { text: "Quelle bourde défensive !", weight: 2 },
   { text: "L'arrière-garde qui flanche !", weight: 2 },
   { text: "Les défenseurs qui perdent pied !", weight: 2 },
+  { text: "Oh ! Le manque de concentration défensive !", weight: 2 },
+  { text: "Que ça manque d'aggressivité derrière !", weight: 2 },
   
   // Rares (poids 0.1)
-  { text: "Défense aux abonnés absents !", weight: 0.1 },
-  { text: "C'est le festival offensif !", weight: 0.1 },
-  { text: "La ligne défensive qui prend l'eau !", weight: 0.1 } // Changé pour éviter doublon
+  { text: "La défense est aux abonnés absents ce soir !", weight: 0.1 },
+  { text: "C'est un festival offensif !", weight: 0.1 },
+  { text: "Ils sont en train de boire la tasse en défense !", weight: 0.1 }
 ];
 
 // Openers pour pattern minimaliste (plus directs)
@@ -84,12 +94,12 @@ const MINIMAL_TEMPLATES = [
   { text: "C'est {scorer} !", weight: 3 },
   
   // Moins communes (poids 2)
-  { text: "{scorer} conclut !", weight: 2 },
-  { text: "{scorer} ajuste !", weight: 2 },
+  { text: "{scorer} à la conclusion !", weight: 2 },
+  { text: "{scorer} ajuste à la perfection !", weight: 2 },
   
   // Rares (poids 0.1)
-  { text: "{scorer} crucifie !", weight: 0.1 },
-  { text: "{scorer} transperce !", weight: 0.1 }
+  { text: "{scorer} à la baguette !", weight: 0.1 },
+  { text: "{scorer} dans ses oeuvres !", weight: 0.1 }
 ];
 
 function weightedRandom(items) {
